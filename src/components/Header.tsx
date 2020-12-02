827af417270d0acde7717d2993bb824f3d0eaaac
Syed Ali Uz Zaman <45460148+alyaxi@users.noreply.github.com>
2020-12-01 16:05:03 -0800

    Header Updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,35 +1,18 @@
 import React from "react";
-import { makeStyles, AppBar, Toolbar, Typography,IconButton} from '@material-ui/core';
+import { AppBar, Toolbar, Typography} from '@material-ui/core';
 
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-      width:"5px"
-    },
-  }));
+
 
 
 export const Header: React.FC<{}> = () => {
    
-      const classes = useStyles();
 
   return (
     <div >
       <AppBar position="static" className="header">
         <Toolbar variant="dense">
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-  
-          </IconButton>
-          <Typography variant="h6" color="inherit">
+          <Typography variant="h5" color="inherit">
           Expense Tracker 
           </Typography>
         </Toolbar>
